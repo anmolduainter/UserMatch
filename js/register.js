6dedcfc82c08e15c46c6d9b195fac5b50cdf399c
anmolduainter <anmolduainter@gmail.com>
2017-07-09 23:44:45 +0530

    register

---
+++ js/register.js
@@ -0,0 +1,79 @@
+/**
+ * Created by anmol on 9/7/17.
+ */
+
+let arr=[];
+let arrId=[];
+let email;
+let username;
+let password;
+let confirmPassword;
+let buttonRegis;
+let id=-1;
+$(function(){
+
+    retrieveToLocal()
+
+  email=$('#email')
+    username=$('#username');
+  password=$('#password');
+  confirmPassword=$('#ConfirmPassword');
+  buttonRegis=$('#register');
+  buttonRegis.click(function () {
+
+      id=id+1;
+      if(checkEmail(email.val())){
+
+          alert('Already Have an Account!!!!!!')
+
+      }
+      else if (password.val()==confirmPassword.val()){
+         Register(id);
+      }else{
+
+          console.log("Error");
+
+      }
+
+  })
+
+});
+
+function objc(id,username,email,password){
+    this.id=id;
+    this.username=username;
+    this.email=email;
+    this.password=password;
+}
+
+function Register(id){
+
+    arr.push(new objc(id,username.val(),email.val(),password.val()));
+   saveToLocal()
+
+}
+
+function saveToLocal(){
+    localStorage.setItem('register',JSON.stringify(arr));
+    window.open("RegisterPref.html","_self")
+}
+
+function retrieveToLocal(){
+    arr=JSON.parse(localStorage.getItem('register'))
+    if (arr){
+        id=arr[arr.length-1].id;
+    }
+    else{
+        arr=[];
+    }
+}
+
+function checkEmail(value){
+
+   for(i in arr){
+       if (value==arr[i].email){
+           return true;
+       }
+   }
+    return false;
+}
